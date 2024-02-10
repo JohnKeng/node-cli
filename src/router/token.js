5eb285c78edcc6656f976d5d14cdae293271df13
John Keng <m0920173456@gmail.com>
2024-02-10 23:35:47 +0800

    Refactor token router to handle GET and POST requests

--- src/router/token.js
+++ src/router/token.js
@@ -5,20 +5,20 @@ const { SuccessModel, ErrorModel } = require('../model/resModel.js')
 const handleTokenRouter = (req, res)=> {
     const {method, path, body, session} = req
 
-    // if(method === 'GET' && path === '/api/get/token'){
-    //     const {username, password} = req.query
-    //     const login = logincheck(username, password)
-    //     return login.then(d=> {
-    //         if(d.username){
-    //             session.token = generateToken()
-    //             session.username = d.username
-    //             session.realname = d.realname
-    //             set(session.token, session) 
-    //             return { username:session.username,token: session.token }
-    //         }
-    //         return new ErrorModel(`登入失敗`)
-    //     })
-    // }
+    if(method === 'GET' && path === '/api/get/token'){
+        const {username, password} = req.query
+        const login = logincheck(username, password)
+        return login.then(d=> {
+            if(d.username){
+                session.token = generateToken()
+                session.username = d.username
+                session.realname = d.realname
+                set(session.token, session) 
+                return { username:session.username,token: session.token }
+            }
+            return new ErrorModel(`登入失敗`)
+        })
+    }
 
     if(method === 'POST' && path === '/api/get/token'){
         const {username, password} = body
@@ -38,7 +38,7 @@ const handleTokenRouter = (req, res)=> {
 
 const generateToken = () => {
     return `${new Date().getTime()}${Math.random()}`
-  }
+}
  
   
 module.exports = handleTokenRouter 
